b35a7f335a5d11cb4158141805076e58cb925628
arghayt0210 <argha.yt.0210@gmail.com>
2024-12-25 00:23:03 +0530

    added headers for Coop error solving

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin-allow-popups',
           },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Resource-Policy',
+            value: 'cross-origin',
+          }
         ],
       },
     ];
